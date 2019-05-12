38b9c2062caaf54564bf7b1005d08edacc13dd49
Timothy Zhang <tsme4wk@gmail.com>
2019-05-12 06:19:40 -0700

    added test function for CV()

---
+++ test/nmf_test.R
@@ -0,0 +1,57 @@
+library(data.table)
+
+# Testing functino for nmf.R:CV(). "OK" will be printed if all test cases 
+#   passed.
+#
+# Arguments: 
+#   - @CV_out: str
+#        String of the path to the .rds data file that contains the CV() output
+#
+#   - @p: double
+#       Portions of the number of jokes per user used for CV(). 
+#       0 < p < 1
+#
+#   - @testing300: str, default 'data/jester-data-testing.csv'
+#       Path to the testing set with the chosen 300 users.
+#
+# Returns: NA
+test_CV <- function(CV_out, p, testing300='data/jester-data-testing.csv')
+{
+  l <- readRDS(CV_out)
+  train <- l[[1]]
+  test <- l[[2]]
+  
+  
+  df_300 <- fread(testing300)
+  testuIDs <- df_300$UserID + 1 # uIDs start from 0 in jester-data-testing.csv
+  
+  curUser <- 0
+  for(u in testuIDs)
+  {
+    combined <- data.table(matrix(nrow=0, ncol=4))
+    colnames(combined) <- c('uID', 'jID', 'rating', 'nRated') 
+    
+    for(i in 1:length(test))
+    {
+      # Testset must NOT be in trainset
+      stopifnot(!(any(test[[i]][uID == u]$jID %in% train[[i]][uID == u]$jID)))
+      # Train size must be p * nRated
+      stopifnot(nrow(train[[i]][uID == u]) == round(p*train[[i]][uID == u]$nRated[1]))
+      combined <- rbind(combined, test[[i]][uID == u])
+    }
+    # combined testset should cover all jIDs
+    stopifnot(nrow(combined$jID) == 100)
+    stopifnot(all(sort(unique(combined$jID)) == 1:100))
+    
+    curUser <- curUser + 1
+    
+    if(curUser %% 100 == 0)
+      cat(paste0(curUser, ' test users done.\n'))
+  }
+  total <- 0
+  for(df in train)
+  {
+    total <- sum(total, nrow(df))
+  }
+  cat('OK\n')
+}
