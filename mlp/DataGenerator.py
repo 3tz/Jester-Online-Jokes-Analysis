054cac84358cc060d9defe867111a6fe641e55b9
Timothy Zhang <tsme4wk@gmail.com>
2019-06-12 12:24:30 -0700

    updated data generator for embeddings

--- mlp/DataGenerator.py
+++ mlp/DataGenerator.py
@@ -9,11 +9,11 @@ JCATS = ['j' + str(i+1) for i in range(MAX_JID)]
 
 np.random.seed(9999)
 
-# Original code by Afshine Amidi & Shervine Amidi from 
+# Original code by Afshine Amidi & Shervine Amidi from
 #   https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
 class DataGenerator(keras.utils.Sequence):
     'Generate data batch by batch for 73,421 categorical input NN'
-    def __init__(self, train, batch_size=1000, shuffle=False):
+    def __init__(self, train, batch_size=4096, shuffle=True):
         'Initialization'
         self.train = train
         self.batch_size = batch_size
@@ -30,15 +30,18 @@ class DataGenerator(keras.utils.Sequence):
         # get the batch DF subset; index = 0, 1, 2, ...
         indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
         df = self.train.iloc[indexes, :]
-        
+
         # Dummify user and joke IDs for this DF subset
-        u_dummies = pd.get_dummies(df['uID'], prefix='u', prefix_sep='')
-        j_dummies = pd.get_dummies(df['jID'], prefix='j', prefix_sep='')
-        u_dummies = u_dummies.reindex(columns=UCATS, fill_value=0)
-        j_dummies = j_dummies.reindex(columns=JCATS, fill_value=0)
-        
-        X = pd.concat([u_dummies, j_dummies], 1).values
-        y = df.iloc[:,2].values
+        # u_dummies = pd.get_dummies(df['uID'], prefix='u', prefix_sep='')
+        # j_dummies = pd.get_dummies(df['jID'], prefix='j', prefix_sep='')
+        # u_dummies = u_dummies.reindex(columns=UCATS, fill_value=0)
+        # j_dummies = j_dummies.reindex(columns=JCATS, fill_value=0)
+        #
+        # X = pd.concat([u_dummies, j_dummies], 1).values
+        # y = df.iloc[:,2].values
+
+        X = [df.uID, df.jID]
+        y = df.iloc[:, 2]
 
         return X, y
 
